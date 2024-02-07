64cf6aebac40f5cb867123b71ed09238edd61510
jayesh sarvaiya <jayeshsarvaiya9913@gmail.com>
2024-02-07 17:06:10 +0530

    first commit

---
+++ day1/target/debug/day1.d
@@ -0,0 +1,1 @@
+C:\Users\jayes\OneDrive\Desktop\rust\day1\target\debug\day1.exe: C:\Users\jayes\OneDrive\Desktop\rust\day1\src\main.rs
---
+++ day1/target/debug/deps/day1.d
@@ -0,0 +1,5 @@
+C:\Users\jayes\OneDrive\Desktop\rust\day1\target\debug\deps\day1.exe: src\main.rs
+
+C:\Users\jayes\OneDrive\Desktop\rust\day1\target\debug\deps\day1.d: src\main.rs
+
+src\main.rs:
